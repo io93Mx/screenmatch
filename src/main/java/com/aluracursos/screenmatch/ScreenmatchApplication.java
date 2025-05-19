d14769ca088f43a67d129e93e0d6338e4abe5379
io93Mx <196264821+io93Mx@users.noreply.github.com>
2025-05-19 15:21:24 -0600

    pom.xml dependency

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
@@ -11,12 +13,24 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	//mi API KEY
+    //http://www.omdbapi.com/?t=matrix&apikey=464d1cb3
+
 	@Override
 	public void run(String... args) throws Exception {
 		//esto es por default pero no nos sirve ahorita
-		//TODO Auto-generated method stub
+		
 		//throw new UnsupportedOperationException("Unimplemented method 'run'");
+		
+		//aparece al principio
 		System.out.println("Hola mundo desde Spring");
+
+		var consumoAPI = new ConsumoAPI();
+		//metodo de consumoAPI.java, espera una String
+		var json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=464d1cb3");
+		//consultar otra API, imagen de cafe aleatoria, aunque ya no esta disopponible
+		//var json = consumoAPI.obtenerDatos("http://www.coffe.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 
 }
