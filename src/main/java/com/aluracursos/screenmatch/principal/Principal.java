ca99d6b239a4fb93951ca7cef2e974ee346932ec
io93Mx <196264821+io93Mx@users.noreply.github.com>
2025-05-25 15:37:44 -0600

    4.8 mapa de datos por temporada

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -78,18 +80,18 @@ public class Principal {
 
 
 
-        //top 5 episodios
-        System.out.println("Top 5 mejores episodios");
+        // //top 5 episodios
+        // System.out.println("Top 5 mejores episodios");
 
-        datosEpisodios.stream()
-        .filter(e -> !e.evalucion().equalsIgnoreCase("N/A"))
-        .peek(e -> System.out.println("Primer filtro (N/A)" + e))
-        .sorted(Comparator.comparing(DatosEpisodio::evalucion).reversed())
-        .peek(e -> System.out.println("Segundo filtro de ordenacion (M>m)" + e))
-        .map(e -> e.titulo().toUpperCase())
-        .limit(5)
-        .peek(e -> System.out.println("Tercer filtro mayusculas (m/M)" + e))
-        .forEach(System.out::println);
+        // datosEpisodios.stream()
+        // .filter(e -> !e.evalucion().equalsIgnoreCase("N/A"))
+        // .peek(e -> System.out.println("Primer filtro (N/A)" + e))
+        // .sorted(Comparator.comparing(DatosEpisodio::evalucion).reversed())
+        // .peek(e -> System.out.println("Segundo filtro de ordenacion (M>m)" + e))
+        // .map(e -> e.titulo().toUpperCase())
+        // .limit(5)
+        // .peek(e -> System.out.println("Tercer filtro mayusculas (m/M)" + e))
+        // .forEach(System.out::println);
 
         //datosEpisodios.stream() permite aplicar operaciones funcionales
         // .filter(e -> !e.evalucion().equalsIgnoreCase("N/A")) filtra todos los que sean diferentes a N/A
@@ -100,6 +102,10 @@ public class Principal {
         
         //¿Es una lambda, .forEach(System.out::println); ?
         //No exactamente, no es una lambda, pero es muy similar. Es una referencia a método (en inglés: method reference), que es una forma más concisa de escribir una lambda que simplemente llama a un método existente.
+
+
+
+
 
 
 
@@ -115,12 +121,12 @@ public class Principal {
 
         //busqueda de episodios a partir de x año
 
-        System.out.println("Indica desde que año dseas buscar los episodios");
-        var fecha = teclado.nextInt();
-        teclado.nextLine();
+        // System.out.println("Indica desde que año dseas buscar los episodios");
+        // var fecha = teclado.nextInt();
+        // teclado.nextLine();
 
-        LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
-        //desde la fecha del usuario, del mes y dia 1
+        // LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+        // //desde la fecha del usuario, del mes y dia 1
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -134,6 +140,42 @@ public class Principal {
         // });
         // //.forEach(e -> {}) es para varias instrucciones
         // //puede ser (e -> System.out.println());
+
+
+
+        // //busca episodios por un pedazo de titulo
+
+        // System.out.println("Escribe el titulo del episodio que quieres ver");
+        // var pedazoTitulo = teclado.nextLine();
+        // //error
+        // //episodios.stream().filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo).toUpperCase()).findFirst();
+        // //opcional, puede o no contener un episodio
+        // Optional<Episodio> episodioBuscado = episodios.stream().filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
+        // .findFirst();
+        // //episodios.stream().filter(e -> e.getTitulo()) 
+        // //Para cada episodio traer el episodio.titulo, traeer el titulo del episodio
+        // //no imprime, debe haber un sout
+        // if (episodioBuscado.isPresent()) {
+        //     System.out.println(" Episodio encontrado");
+        //     System.out.println(" Los datos son: " + episodioBuscado.get());//trae todos los datos
+        //     //episodioBuscado.getTitulo() trae solo el titulo
+        // } else {
+        //     System.out.println("Episodio no encontrado");
+        // }
+
+
+
+        //
+        Map<Integer, Double> evaluacionesPorTemporada = episodios.stream()
+        .filter(e -> e.getEvaluacion() > 0.0)
+        .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getEvaluacion)));
+        System.out.println(evaluacionesPorTemporada);
+        //.filter(e -> e.getEvaluacion() > 0.0) filtra las que son mayores de 0.0
+
+
+
+
+
 
     }
 
